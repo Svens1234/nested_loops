017009c029b9b23695412d679b2e0db63d3e4a93
Svenden123 <buhrus1@yandex.ru>
2021-08-03 19:34:15 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#for i in range(3):
+    #for j in range(3):
+        #print(i, j)
+
+
+#cars = ["mazda", "opel", "audi", "honda"]
+#for i in range(len(cars)):
+    #for h in range(i+1):
+      #print(cars[i])
+
+
+for n in range(1, 4):
+    for j in ["a", "b", "c"]:
+        print("n =", n, "and j =", j)
